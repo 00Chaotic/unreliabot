ac200fd373609551fe69157727dbec2574c9ed2b
Jaffar Rizvi <J.Rizvi@outlook.com>
2023-05-23 00:45:58 +1000

    De-pluralise table

--- db/migrations/20230522110843-create-auth-tokens.js
+++ db/migrations/20230522110843-create-auth-tokens.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('auth_tokens', {
+    await queryInterface.createTable('auth_token', {
       user_id: {
         allowNull: false,
         primaryKey: true,
@@ -29,6 +29,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('auth_tokens');
+    await queryInterface.dropTable('auth_token');
   }
 };
